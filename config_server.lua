1f3e336461a21466cb1dfa0878d3a266ed4f5ae0
qwexzy <65239981+qwexzy@users.noreply.github.com>
2023-05-19 00:19:47 +0200

    Add files via upload

---
+++ config_server.lua
@@ -0,0 +1,11 @@
+Config.Server = {}
+
+Config.Server.webhooks = {
+	all = "DISCORD_WEBHOOK",
+	chat = "DISCORD_WEBHOOK",
+	joins = "DISCORD_WEBHOOK",
+	leaving = "DISCORD_WEBHOOK",
+	deaths = "DISCORD_WEBHOOK",
+	shooting = "DISCORD_WEBHOOK",
+	resources = "DISCORD_WEBHOOK",
+}
